5af6d932ef1db64a67b1d3c5a67a16fa2a5a6902
ykshdja <ykhanduja1@gmail.com>
2022-10-22 01:10:47 -0400

    Stage-2

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 /**
  * STATEMENT OF AUTHORSHIP - I YASH KHANDUJA, 000826385 HERE BY DECLARE THAT THIS IS MY OWN WORK.
+ * Stage 1
+ * Stage 2
  * **/
 
   
@@ -7,7 +9,9 @@
 // digital input pin definitions
 #define PIN_PIR D5
 #define PIN_BUTTON D6
-#define TIME 10000
+#define setLed(b) (digitalWrite(LED_BUILTIN,b?LOW:HIGH))
+#define TIME 100
+#define BLINK_NUM 40
 /**
  * Stage 1:
   When motion is detected, turn the LED on solid for 10 seconds, then turn it off and wait for another motion 
@@ -22,6 +26,13 @@ bool detectMotion(){
     previousPirState = currPirState;
     return motion;
 }
+bool lastBtnState = false;
+bool PushedBtn(){
+    bool currBtnState = digitalRead(PIN_BUTTON) == LOW;
+    bool pushed = (currBtnState && lastBtnState);
+    lastBtnState = currBtnState;
+    return pushed;
+}
 
 void _SetLed_(bool state){
     if(state == true){
@@ -33,22 +44,35 @@ void _SetLed_(bool state){
     }
 }
 
-bool lastBtnState = false;
-bool PushedBtn(){
-    bool currBtnState = digitalRead(PIN_BUTTON) == LOW;
-    bool pushed = (currBtnState && lastBtnState);
-    lastBtnState = currBtnState;
-    return pushed;
+void handleMotionDetection(){
+    for(int i=0;i<BLINK_NUM;i++){
+        _SetLed_(true);
+        delay(50);
+        _SetLed_(false);
+        delay(200);
+    }
+    Serial.println("*********************************");
+    Serial.print("Motion Detected at - ");
+    Serial.print(millis()/1000);
+    Serial.println(" Seconds");
 }
 
 
-void setup() {
-  Serial.begin(115200);
 
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  Serial.println("=========================================");
+  Serial.println("---ALARM SYSTEM - STAGE 2---");
+  Serial.println("Yash Khanduja, 000826385");
+  Serial.println("=========================================");
+ 
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PIN_PIR, INPUT);
   pinMode(PIN_BUTTON, INPUT_PULLUP);
-  _SetLed_(false);
+  setLed(false);
   Serial.print("calibrating sensor ");
     for(int i = 0; i <10; i++){
       Serial.print(".");
@@ -61,17 +85,14 @@ void setup() {
 }
 
 void loop() {
-   
-    if(detectMotion()){
-        _SetLed_(true);
-        Serial.println("------");
-        Serial.print("Motion Detected - ");
-        Serial.print(millis()/1000);
-        Serial.println(" Seconds");
+    _SetLed_(false);
+    bool detected = detectMotion();
+    if(detected){
+        handleMotionDetection();
         delay(TIME);
-        _SetLed_(false);
+       
     }
-    
-     delay(100);
+     _SetLed_(false);
+         delay(100);
 
 }
