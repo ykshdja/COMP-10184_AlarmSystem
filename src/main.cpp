f020be4b201b1e8ef3c496999374641a61bf97fb
ykshdja <ykhanduja1@gmail.com>
2022-10-23 01:12:35 -0400

    Stage 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 /**
  * STATEMENT OF AUTHORSHIP - I YASH KHANDUJA, 000826385 HERE BY DECLARE THAT THIS IS MY OWN WORK.
- * Stage 1
- * Stage 2
+ * Stage 4
  * **/
 
   
 #include <Arduino.h>
-// digital input pin definitions
 #define PIN_PIR D5
 #define PIN_BUTTON D6
 #define TIME 100
@@ -26,33 +24,37 @@ Stage 3:
  Like stage 2, but after 10s, turn on the LED solid and keep it that way. Only by resetting the D1 Mini will the
  program wait for more motion events.
 
+ Stage 4:
+ Like stage 3, but if the pushbutton is pressed while the LED is flashing, turn the LED off and wait for another
+ motion detection event.
+
 
 */
 
 enum alarmState{
-    enabled, //0
-    countdown, //1
-    active, //2
-    disabled //3
+    enabled, 
+    countdown, 
+    active, 
+    disabled 
 };
-
 int isAlarmActive = enabled;
+long countDown = 0;
+bool previousPirState = false;
+bool lastBtnState = false;
+
 void handleAlarmEnabled(bool isDetected){
     if(isDetected == true){
         isAlarmActive = countdown;
     }
-
-
 }
 
-bool previousPirState = false;
 bool detectMotion(){
     bool currPirState = digitalRead(PIN_PIR) == HIGH;
     bool motion = (currPirState && !previousPirState);
     previousPirState = currPirState;
     return motion;
 }
-bool lastBtnState = false;
+
 bool PushedBtn(){
     bool currBtnState = digitalRead(PIN_BUTTON) == LOW;
     bool pushed = (currBtnState && lastBtnState);
@@ -73,9 +75,9 @@ void _SetLed_(bool state){
 void handleMotionDetection(){
     for(int i=0;i<BLINK_NUM;i++){
         _SetLed_(true);
-        delay(50);
+        delay(100);
         _SetLed_(false);
-        delay(200);
+        delay(100);
     }
     isAlarmActive = active;
     Serial.println("*********************************");
@@ -84,16 +86,40 @@ void handleMotionDetection(){
     Serial.println(" Seconds");
 }
 
+void handlebtnPressOnFlashing(bool btnPushed){
+    if(countDown == 0){
+        countDown = millis();
+    }
+    if(btnPushed){
+        _SetLed_(false);
+        countDown = 0;
+        isAlarmActive = enabled;
+    Serial.println("****************");
+    Serial.print("Button Pushed at - ");
+    Serial.print(millis()/1000);
+    Serial.println(" Seconds");
+    return;
+    }
+    int timeCountDownStart = millis() - countDown;
+    if(timeCountDownStart>(200*40)){
+        countDown = 0;
+        isAlarmActive = active;
+        Serial.println("****************");
+        Serial.print("Motion Detected at - ");
+        Serial.print(millis()/1000);
+        Serial.println(" Seconds");
+        return;
+    }
+    _SetLed_(timeCountDownStart % 200 < 50);
 
-
+}
 
 void setup() {
-
   Serial.begin(115200);
   Serial.println();
   Serial.println();
   Serial.println("=========================================");
-  Serial.println("---ALARM SYSTEM - STAGE 2---");
+  Serial.println("===ALARM SYSTEM - STAGE 4===");
   Serial.println("Yash Khanduja, 000826385");
   Serial.println("=========================================");
  
@@ -109,38 +135,22 @@ void setup() {
     Serial.println(" done");
     Serial.println("SENSOR ACTIVE");
     delay(50);
-
 }
 
 void loop() {
     bool detected = detectMotion();
+    bool pushDetected = PushedBtn();
     switch(isAlarmActive){
         case enabled:
         handleAlarmEnabled(detected);
         break;
         case countdown:
-         handleMotionDetection();
+         handlebtnPressOnFlashing(pushDetected);
          break;
          case active:
          _SetLed_(true);
          break;
-
+         case disabled:
+         break;
     }
-
-
-
-
-
-
-
-    // _SetLed_(false);
-    // bool detected = detectMotion();
-    // if(detected){
-    //     handleMotionDetection();
-    //     delay(TIME);
-    //     return;
-    // }
-    // setLed(true);
-    //      delay(100);
-
 }
